e02aafe79e79b7cd56a16538c17b3591e60255a6
Benito <b.farina5@studenti.unisa.it>
2024-04-19 20:27:07 +0200

    Aggiunta di vincoli al Database

--- OhMyBag/db/OhMyBag.sql
+++ OhMyBag/db/OhMyBag.sql
@@ -18,63 +18,63 @@ IndirizzoSpedizione varchar(100) not null
 CREATE TABLE Carta(
 Id 				int 		 Primary key,
 Username		varchar(100) not null,
-CVV				int			 not null,
+CVV				int			 check(CVV>=0)not null,
 Circuito		varchar(100) not null,
 DataScadenza	date 		 not null,
-NumeroCarta 	int 		 not null,
+NumeroCarta 	varchat(25)  not null,
 foreign key (Username) references Utente(Username)
 );
 
 CREATE TABLE Ordine(
-Id 						int 		 Primary key,
-PrezzoTotale			float		 not null,
-Destinatario			varchar(100) not null,
-MetodoDiPagamento		varchar(100) not null,
-IndirizzoDiSpedizione	varchar(100) not null,
-NoteCorriere			varchar(500) not null,
-MetodoDiSPedizione		varchar(100) not null,
-NumeroTracking			varchar(100) not null,
-Data					date 		 not null,
-Circuito				varchar(100) not null,
-ConfezioneRegalo		boolean 	 not null,
-NumeroCarta				int 		 not null,
-Username				varchar(100) not null,
+Id 						int 		 	Primary key,
+PrezzoTotale			float		 	check(PrezzoTotale>=0)not null,
+Destinatario			varchar(100) 	not null,
+MetodoDiPagamento		varchar(100) 	not null,
+IndirizzoDiSpedizione	varchar(100) 	not null,
+NoteCorriere			varchar(1000)	not null,
+MetodoDiSPedizione		varchar(20) 	check(MetodoDiSpedizione IN ("Standard", "Premium"))not null,
+NumeroTracking			varchar(100) 	not null,
+Data					date 		 	not null,
+Circuito				varchar(100) 	not null,
+ConfezioneRegalo		boolean 	 	not null,
+NumeroCarta				varchar(25) 	not null,
+Username				varchar(100) 	not null,
 foreign key (Username) references Utente(Username)
 );
 
 CREATE TABLE Categoria(
-	ID 	 int 			Primary Key,
-    Nome varchar(100)	not null    /*Donna, Uomo, Viaggi*/
+	ID 	 int 			check(ID IN (01,02,03))Primary Key,
+    Nome varchar(10)	check(Nome IN ("Donna","Uomo","Viaggi"))not null
 );
 
 CREATE TABLE Prodotto(
 	ID 				varchar(100)  Primary Key,
     Marca 			varchar(100)  not null,
     Nome 			varchar(100)  not null,
-    Prezzo 			float 		  not null,
+    Prezzo 			float 		  check(Prezzo>=0)not null,
     Tipologia 		varchar(100)  not null,
-    IDCategoria 	int 		  not null,
+    IDCategoria 	int 		  check(IDCategoria IN (01,02,03))not null,
     Descrizione 	varchar(2000) not null,
-    AnnoCollezione  int 		  not null,
+    AnnoCollezione  int 		  check(AnnoCollezione>=1900)not null,
     DataInserimento Date 		  not null,
-    Sconto 			int,
-    Disponibilita 	int, 
+    Sconto 			int 		  check(Sconto>=0 && Sconto<100),
+    Disponibilita 	int 		  check(Disponibilita>=0),
     foreign key (IDCategoria) references Categoria(ID)
 );
 
 CREATE TABLE Composizione(
 	IDOrdine 	int 			not null,
     IDProdotto  varchar(100) 	not null,
-    Prezzo 		float 			not null,
-    Quantita 	int 			not null,
-    IVA 		float 			not null,
+    Prezzo 		float 			check(Prezzo>=0)not null,
+    Quantita 	int 			check(Quantita>=0)not null,
+    IVA 		float 			check(IVA>=0)not null,
     foreign key (IDOrdine) references Ordine(ID),
     foreign key (IDProdotto) references Prodotto(ID)
 );
 
 CREATE TABLE Immagine(
 	Nome 	   varchar(100) 	Primary key,
-    Copertina  boolean,
+    Copertina  boolean			not null,
     IDProdotto varchar(100) 	not null,
     foreign key (IDProdotto) references Prodotto(ID)
 );

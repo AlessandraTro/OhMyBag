c3a4bf0a61f8d96b2c96d53853b9c91434ecb642
alessandra <alessandra.trotta2003@gmail.com>
2024-04-21 16:37:25 +0200

    Insert donna

---
+++ OhMyBag/db/InserimentoDati.sql
@@ -0,0 +1,296 @@
+USE OhMyBag;
+
+
+/* INSERIMENTO CATEGORIA */
+INSERT INTO Categoria (ID, Nome) VALUES (01, "Donna");
+INSERT INTO Categoria (ID, Nome) VALUES (02, "Uomo");
+INSERT INTO Categoria (ID, Nome) VALUES (03, "Viaggi");
+
+
+/* INSERIMENTO DONNA */
+
+/* INSERIMENTO BORSE */
+
+/* GUCCI DIANA */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita) 
+VALUES ("678842 FACPO 5743", "GUCCI", "BORSA SHOPPING GUCCI DIANA MISURA MEDIA", 4500.00, "Borsa", 01, 
+"La collezione Cruise 24 trae ispirazione dai motivi iconici della Maison, reinterpretando simboli ed elementi inconfondibili in modi sempre nuovi. Stagione dopo stagione, l’heritage Gucci caratterizza design, materiali e modelli contemporanei.<br>
+<ul>
+  <li><b>Materiale:</b> Pelle</li>
+  <li><b>Colore:</b> Beige chiaro</li>
+  <li><b>Finiture:</b> anticate color oro lucido</li>
+  <li><b>Logo:</b> Gucci in rilievo</li>
+  <li><b>Fodera:</b> in cotone e lino</li>
+  <li><b>Tasca interna:</b> aperta e due scomparti con cerniera</li>
+  <li><b>Manico:</b> altezza (luce) 10 cm</li>
+  <li><b>Tracolla:</b> in tessuto, altezza (luce) 63 cm, e tracolla in pelle, altezza (luce) 58 cm</li>
+  <li><b>Peso:</b> circa 1,256 kg</li>
+  <li><b>Misura media:</b> L 35 cm x A 30 cm x P 14 cm</li>
+  <li><b>Fabbricato in Italia</b></li>
+  <li>Può contenere iPhone Pro Max/Plus, Airpods, portafoglio piccolo e rossetto</li>
+</ul>", 
+2024, '2024-04-13', 0, 10);
+
+
+/* VERSACE MEDUSA '95 */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita)
+VALUES("1013169-1A10795_DTU_UNICA_1B00V__", "VERSACE", "SHOPPER PICCOLA MEDUSA '95", 1700.00, "Borsa", 01,
+"Shopper orizzontale con dettaglio cintura caratterizzata dalla personalizzazione Medusa '95, l'iconico codice della Maison visto per la prima volta nelle collezioni del 1995. Il design del manico superiore è realizzato in pelle liscia a grana piatta con chiusura magnetica e presenta una tracolla rimovibile.<br>
+<ul>
+  <li><b>Personalizzazione Medusa '95</b></li>
+  <li><b>Logo Versace in rilievo</b></li>
+  <li><b>Manici</b></li>
+  <li><b>Chiusura magnetica</b></li>
+  <li><b>Taschina interna</b></li>
+  <li><b> Colore: </b> Nero + Oro </li>
+  <li><b>Tracolla rimovibile:</b> L91,5 -100 cm</li>
+  <li><b>Altezza (luce) della tracolla:</b> 12 cm</li>
+  <li><b>Piedini in metallo</b></li>
+  <li><b>Composizione esterno:</b> 100% pelle di vitello</li>
+  <li><b>Fodera:</b> 100% pelle di agnello</li>
+  <li><b>Peso approssimativo:</b> 0,5 kg</li>
+  <li>L 23 x L 8 x A 14 cm</li>
+</ul>", 
+2024, '2024-04-13', 0, 10);
+
+
+/*CARPISA BORSA SHOPPING - ATHENA*/
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita)
+VALUES("BSB99201444", "CARPISA", "Borsa Shopping - Athena", 49.95, "Borsa", 01,
+"Shopping della linea Athena by Carpisa in similpelle granato sfoderata. Il modello è dotato di chiusura con zip, una tasca interna, due ampi manici e una tracolla regolabile e removibile in similpelle liscia in tinta. La shopping è pratica, capiente e comoda.<br>
+<ul>
+  <li><b>Linea:</b> ATHENA</li>
+  <li><b>Materiale:</b> Sintetico</li>
+  <li><b>Colore:</b> Nero</li>
+  <li><b>Made in:</b> Cina</li>
+  <li><b>Collezione:</b> Primavera/Estate 2024</li>
+  <li><b>Dimensioni (cm):</b> 35.5 x 28.5 x 14</li>
+</ul>", 
+2024, '2024-04-13', 0, 10);
+
+
+/* ARMANI Borsa hobo a spalla in pelle stampa cervo */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita)
+VALUES("Y3E205Y497O180100", "ARMANI", "Borsa hobo a spalla in pelle stampa cervo", 550.00, "Borsa", 01,
+"Linee pulite super minimal con piccoli tocchi di metallo. L’ispirazione di queste borse dal tipico design hobo, nasce dal trend delle borse sottospalla, dai volumi decisi e dalla silhouette particolarmente geometrica. Borsa a spalla realizzata in pelle con finish stampa cervo e provvista di chiusura con linguetta di pelle che viene inserita all’interno di un passante di metallo logato.<br>
+<ul>
+  <li><b>Dettagli metallici:</b> gold</li>
+  <li><b>Dimensioni:</b> 25 x 36 x 7 cm</li>
+  <li><b>Composizione:</b> 100% Pelle di bovino</li>
+  <li><b>Materiale: </b> Pelle</li>
+  <li><b>Brand logo</b></li>
+  <li><b>Colore:</b> Tortora, tinta unita</li>
+  <li><b>Manico:</b> singolo</li>
+  <li><b>Interno foderato</b></li>
+  <li><b>Tasca interna con zip</b></li>
+</ul>", 
+2024, '2024-04-13', 0, 10);
+
+
+/* PADRA Borsa a tracolla Prada Re-Edition 1978 mini in Re-Nylon e Saffiano */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita)
+VALUES ("1BH218_R064_F0594_V_OOO", "PRADA", "Borsa a tracolla Prada Re-Edition 1978 mini in Re-Nylon e Saffiano", 1900.00, "Borsa", 01,
+"Re-Edition di un design d’archivio, questa borsa a tracolla dal fascino senza tempo sintetizza i codici Prada in una silhouette pragmatica e sofisticata. Il modello essenziale dalle geometrie pulite è realizzato in Re-Nylon, tessuto innovativo in nylon rigenerato, evoluzione sostenibile di un materiale distintivo del marchio. Dotato di una tasca esterna con zip e impreziosito da eleganti dettagli in pelle Saffiano, l’accessorio è completato dall’iconico del logo triangolo in metallo smaltato.<br>
+<ul>
+  <li><b>Tracolla regolabile in Saffiano.</b> Lunghezza massima 125 cm - Lunghezza minima 115 cm - Luce massima 60 cm- Luce minima 55 cm</li>
+  <li><b>Portachiavi Re-Edition rimovibile in pelle</b></li>
+  <li><b>Tasca frontale con zip</b></li>
+  <li><b>Finiture in metallo</b></li>
+  <li><b>Logo su triangolo in metallo smaltato frontale</b></li>
+  <li><b>Chiusura zip</b></li>
+  <li><b>Colore:</b> Sughero</li>
+  <li><b>Interno in Re-Nylon siglato con tasca zip</b></li>
+  <li><b>Altezza:</b> 17cm</li>
+  <li><b>Larghezza:</b> 21cm</li>
+  <li><b>Lunghezza:</b> 8cm</li>
+  <li><b>Materia:</b> Tessuto/Pelle</li>
+</ul>
+<i>Per una cura sostenibile del prodotto, per preservarne le caratteristiche e per ridurre la dispersione di microfibre, si consiglia di non lavare il prodotto troppo spesso, di far prendere aria dopo ogni utilizzo e affidare il lavaggio a secco ad una tintoria specializzata ed ecologica.</i>", 
+2024, '2024-04-13', 0, 10);
+
+
+
+/* INSERIMENTO ZAINI */
+
+/* CALVIN KLEIN ZAINO ROTONDO */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita) 
+VALUES("0000PH0700010", "CALVIN KLEIN", "Zaino Rotondo", 76.00, "Zaino", 01, 
+"L'esterno dello zaino è realizzato al 100% in poliestere riciclato, escluse le finiture. Il poliestere riciclato di questo prodotto è stato ottenuto dalla rielaborazione di materiali esistenti.<br>
+<ul>
+  <li><b>Esterno in poliestere riciclato resistente all'acqua</b></li>
+  <li><b>Manico superiore e spalline regolabili</b></li>
+  <li><b>Lunghezza della tracolla regolabile:</b> 49 cm - 91 cm</li>
+  <li><b>Modello con zip integrale</b></li>
+  <li><b>Tasca esterna sul davanti</b></li>
+  <li><b>Sostegno imbottito in rete</b></li>
+  <li><b>A:</b> 45 cm <b>L:</b> 30 cm <b>l:</b> 12 cm</li>
+  <li><b>Logo Calvin Klein sul davanti</b></li>
+  <li><b>Colore:</b> Nero</li>
+  <li>100% poliestere riciclato</li>
+  <li>Paese d'origine: Cina</li>
+</ul>", 
+2023, '2024-04-16', 10, 10);
+
+
+
+
+/* INSERIMENTO CINTURE */
+
+/* TOMMY HILFIGER CINTURA TH MONOGRAM IN PELLE */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita) 
+VALUES ("AW0AW15763AES", "TOMMY HILFIGER", "CINTURA TH MONOGRAM IN PELLE", 45.00, "Cintura", 01, 
+"Il nuovo segno distintivo unisce con genialità tradizione e innovazione. Il monogramma rivisita in chiave moderna un'icona inconfondibile del brand. L'elegante cintura in pelle aggiunge una nota di lusso al tuo look, grazie al brillante monogramma TH in metallo sul passante.<br>
+<ul>
+  <li><b>Finitura liscia</b></li>
+  <li><b>Fibbia quadrata in metallo</b></li>
+  <li><b>Placchetta con monogramma TH sul passante</b></li>
+  <li><b>Branding Tommy Hilfiger</b></li>
+  <li><b>Colore:</b> white clay</li>
+  <li><b>Larghezza:</b> 2,5 cm</li>
+  <li><b>Materiale:</b> 100% pelle</li>
+</ul>", 
+2023, '2024-04-16', 30, 10);
+
+
+/* INSERIMENTO PORTAFOGLI */
+
+/* LOUIS VUITTON PORTAFOGLIO SARAH*/
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita) 
+VALUES("M60531", "LOUIS VUITTON", "Portafoglio Sarah", 530.00, "Portafoglio", 01, 
+"Il portafoglio Sarah, grande classico di Louis Vuitton, combina la tela Monogram con una fodera in pelle martellata. Il raffinato accessorio, con apertura a busta, rivela due spaziosi scomparti, pratiche tasche e fessure per carte di credito. La creazione si distingue per il design elegante e funzionale, perfetto per contenere l'essenziale.<br>
+<ul>
+  <li><b>Dimensioni:</b> 19 x 10.5 x 2.5 cm (Lunghezza x Altezza x Larghezza)</li>
+  <li><b>Esterno in tela Monogram</b></li>
+  <li><b>Colore:</b> Marrone</li>
+  <li><b>Fodera</b> in vacchetta pieno fiore</li>
+  <li>Parti metalliche dorate</li>
+  <li>Chiusura con bottone a pressione</li>
+  <li>Tasca piatta esterna</li>
+  <li>Sedici fessure per carte di credito</li>
+  <li>Scomparto con zip per monete</li>
+  <li>Due ampi scomparti a soffietto</li>
+  <li>Tasca piatta interna</li>
+</ul>
+L'articolo può essere prodotto in Francia, Spagna, Italia o negli Stati Uniti.", 
+2024, '2024-04-17', 0, 10);
+
+
+/* INSERIMENTO SCIARPE E CAPPELLI */
+
+/* LOUIS VUITTON SCIARPA THE ULTIMATE */
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita) 
+VALUES ("M76382","LOUIS VUITTON","Sciarpa The Ultimate", 890.00, "Sciarpe e cappelli", 01, 
+"La sciarpa The Ultimate è decorata con il motivo Monogram oversize in colori a contrasto, dallo stile decisamente originale. L'accessorio, dalle dimensioni generose, è realizzato in pregiato misto cachemire e lana. La maestria artigianale della Maison è messa in risalto dall'abile uso della tecnica jacquard, che crea il particolare gioco cromatico del raffinato modello.<br>
+<ul>
+  <li><b>Dimensioni:</b> 200 x 70 cm (Lunghezza x Altezza)</li>
+  <li><b>Tessuto:</b> 50% cachemire, 50% lana</li>
+  <li><b>Colore:</b> Beige</li>
+  <li>Trama jacquard</li>
+  <li>Motivo Monogram</li>
+  <li>Bordo sfrangiato<</li>
+  <li>Firma Louis Vuitton</li>
+  <li> Solo lavaggio a secco</li>
+</ul>",
+2024, '2024-04-17', 0, 10);
+
+
+/* INSERIMENTO ACCESSORI */
+
+/* CARPISA PORTACELLULARE*/
+
+INSERT INTO Prodotto (ID, Marca, Nome, Prezzo, Tipologia, IDCategoria, Descrizione, AnnoCollezione, DataInserimento, Sconto, Disponibilita) 
+VALUES("AAC06204444", "CARPISA", "Portacellulare - Vanity Spring", 19.95, "Accessori", 01, 
+"Portacellulare della linea Vanity Spring by Carpisa realizzato in similpelle effetto lucertolina.
+Dotato di chiusura principale con cerniera, due taschine esterne, tira lampo in metallo logato e tracolla in nastro regolabile e removibile. Il modello è disponibile in vari colori pastello che ricordano la primavera.<br>
+<ul>
+  <li><b>Linea:</b> VANITY SPRING</li>
+  <li><b>Materiale:</b> Sintetico</li>
+  <li><b>Colore:</b> Nero</li>
+  <li><b>Made in:</b> Cina</li>
+  <li><b>Collezione:</b> Primavera/Estate 2024</li>
+  <li><b>Dimensioni (cm):</b> 10 x 17 x 2</li>
+</ul>", 
+2024, '2024-04-18', 0, 10);
+
+
+
+
+/* INSERIMENTO IMMAGINI */
+
+/* BORSE */
+
+/* GUCCI DIANA */
+
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Gucci/Borsa Shopping Gucci Diana Misura Media/Pelle Beige Chiaro/1.jpg", 1, "678842 FACPO 5743");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Gucci/Borsa Shopping Gucci Diana Misura Media/Pelle Beige Chiaro/2.jpg", 0, "678842 FACPO 5743");
+
+
+
+/* VERSACE MEDUSA '95 */
+
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Versace/Shopper Piccola Medusa '95/Nero e Oro/1.jpg", 1, "1013169-1A10795_DTU_UNICA_1B00V__");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Versace/Shopper Piccola Medusa '95/Nero e Oro/2.jpg", 0, "1013169-1A10795_DTU_UNICA_1B00V__");
+
+/*CARPISA BORSA SHOPPING - ATHENA*/
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Carpisa/Borsa Shopping/Ardesia/1.jpg", 1,"BSB99201444"); 
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Carpisa/Borsa Shopping/Ardesia/2.jpg", 0,"BSB99201444"); 
+
+
+/* ARMANI Borsa hobo a spalla in pelle stampa cervo */
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Armani/Borsa hobo a spalla in pelle stampa cervo/Tortora/1.jpg", 1, "Y3E205Y497O180100");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Armani/Borsa hobo a spalla in pelle stampa cervo/Tortora/2.jpg", 0, "Y3E205Y497O180100");
+
+
+
+/* PADRA Borsa a tracolla Prada Re-Edition 1978 mini in Re-Nylon e Saffiano */
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Prada/Borsa a tracolla Prada Re-Edition 1978 mini in Re-Nylon e Saffiano/Sughero/1.jpg", 1,"1BH218_R064_F0594_V_OOO");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/borse/Prada/Borsa a tracolla Prada Re-Edition 1978 mini in Re-Nylon e Saffiano/Sughero/2.jpg", 0,"1BH218_R064_F0594_V_OOO");
+
+
+/* ZAINI */
+
+/* CALVIN KLEIN ZAINO ROTONDO */
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/zaini/Calvin Klein/Zaino Rotondo/Nero/1.jpg", 1,"0000PH0700010"); 
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/zaini/Calvin Klein/Zaino Rotondo/Nero/2.jpg", 0,"0000PH0700010"); 
+
+
+
+/* CINTURE */
+
+/* TOMMY HILFIGER CINTURA TH MONOGRAM IN PELLE */
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/cinture/Tommy Hilfiger/Cintura TH Monogram in pelle/White Clay/1.jpg", 1, "AW0AW15763AES");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/cinture/Tommy Hilfiger/Cintura TH Monogram in pelle/White Clay/2.jpg", 0, "AW0AW15763AES");
+
+
+/* PORTAFOGLI */
+
+/* LOUIS VUITTON PORTAFOGLIO SARAH*/
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/portafogli/Louis Vuitton/Portafoglio Sarah/Marrone/1.jpg", 1, "M60531");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/portafogli/Louis Vuitton/Portafoglio Sarah/Marrone/2.jpg", 0, "M60531");
+
+
+/* SCIARPE E CAPPELLI */
+
+/* LOUIS VUITTON SCIARPA THE ULTIMATE */
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/sciarpe_e_cappelli/Louis Vuitton/Sciarpa The Ultimate/Beige/1.jpg", 1, "M76382");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/sciarpe_e_cappelli/Louis Vuitton/Sciarpa The Ultimate/Beige/2.jpg", 0, "M76382");
+
+
+/* ACCESSORI */
+
+/* CARPISA PORTACELLULARE*/
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/accessori/Carpisa/Portacellulare - Vanity Spring/Nero/1.jpg", 1, "AAC06204444");
+INSERT INTO Immagine (Nome, Copertina, IDProdotto) VALUES("img/prodotti/donna/accessori/Carpisa/Portacellulare - Vanity Spring/Nero/2.jpg", 0, "AAC06204444");
+
+
+
+
+

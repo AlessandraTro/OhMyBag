9acf439e739ee24e1889c04ab183b4a8e08c9bb8
alessandra <alessandra.trotta2003@gmail.com>
2024-06-14 15:55:44 +0200

    modifiche ad admin control

--- OhMyBag/src/it/ohmybag/controller/AdminControl.java
+++ OhMyBag/src/it/ohmybag/controller/AdminControl.java
@@ -1,4 +1,4 @@
- package it.ohmybag.controller;
+package it.ohmybag.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -18,14 +18,8 @@ import it.ohmybag.bean.*;
  */
 @WebServlet("/AdminControl")
 public class AdminControl extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-	static ProdottoModel prodottiModel;
-
-	static {
-
-		prodottiModel=new ProdottoModel();
-	}
 
 	public AdminControl() {
 		super();
@@ -34,34 +28,6 @@ public class AdminControl extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String action= request.getParameter("action");
-		String parametro = (String) request.getParameter("ID");
-		
-		try {
-			if(action != null) {
-				switch(action.toLowerCase()){
-					case ("delete"):
-						prodottiModel.deleteProduct(parametro);
-						System.out.println("Eliminato");
-						request.getSession().setAttribute("products", prodottiModel.allProduct());
-						break;
-					case ("detail"):
-						Prodotto prodotto=prodottiModel.doRetrieveById(parametro);
-						String descrizione=prodotto.getDescrizione();
-						request.setAttribute("descrizione",descrizione);
-						break;
-					case ("moddesc"):
-						prodottiModel.updateDescription(parametro,request.getParameter("modDescrizione"));
-						request.getSession().setAttribute("products", prodottiModel.allProduct());
-						System.out.println("Aggiornato");
-						break;
-				}
-			}
-				
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/admin.jsp");
 		dispatcher.forward(request, response);
